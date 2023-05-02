bf2b1bceefe2b28815746d055a4670c2cf28f877
17robots <mdray@ameritech.net>
2023-05-02 17:11:49 -0400

    fix: move gtk to config block

--- home/default.nix
+++ home/default.nix
@@ -81,39 +81,39 @@ with lib; let
   '';
 in {
   config = {
+    gtk = {
+      enable = true;
+      theme = {
+        name = "";
+        package = pkgs.catppuccin-gtk.override {
+          size = "compact";
+          tweaks = ["black" "rimless"];
+        };
+      };
+      iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.papirus-icon-theme;
+      };
+      font = {
+        name = "Lexend";
+        size = 13;
+      };
+      gtk3.extraConfig = {
+        gtk-xft-antialias = 1;
+        gtk-xft-hinting = 1;
+        gtk-xft-hintstype = "hintslight";
+        gtk-xft-rgba = "rgb";
+      };
+      gtk2.extraConfig = ''
+        gtk-xft-antialias = 1
+        gtk-xft-hinting = 1
+        gtk-xft-hintstype = "hintslight"
+        gtk-xft-rgba = "rgb"
+      '';
+    };
     home = {
       stateVersion = "22.11";
     };
-  };
-  gtk = {
-    enable = true;
-    theme = {
-      name = "";
-      package = pkgs.catppuccin-gtk.override {
-        size = "compact";
-        tweaks = ["black" "rimless"];
-      };
-    };
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
-    font = {
-      name = "Lexend";
-      size = 13;
-    };
-    gtk3.extraConfig = {
-      gtk-xft-antialias = 1;
-      gtk-xft-hinting = 1;
-      gtk-xft-hintstype = "hintslight";
-      gtk-xft-rgba = "rgb";
-    };
-    gtk2.extraConfig = ''
-      gtk-xft-antialias = 1
-      gtk-xft-hinting = 1
-      gtk-xft-hintstype = "hintslight"
-      gtk-xft-rgba = "rgb"
-    '';
   };
   home = {
     packages = with pkgs; [
