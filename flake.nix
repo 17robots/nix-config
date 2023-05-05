f39aee2721bf335079308f893bef105cbcd7c3e4
17robots <mdray@ameritech.net>
2023-05-05 00:01:27 -0400

    fix: add hardware input

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     nixos--hardware.url = "github:Nixos/nixos-hardware/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs, home-manager }: let 
+  outputs = { self, nixpkgs, home-manager, nixos-hardware }: let 
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
