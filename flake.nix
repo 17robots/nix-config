726e51057a033fecd64ff20f5c33cbd77dbeb6f1
17robots <mdray@ameritech.net>
2023-05-05 15:26:12 -0400

    fix: inherit home fn

--- flake.nix
+++ flake.nix
@@ -23,10 +23,7 @@
           home-manager = {
             useUserPackages = true;
             useGlobalPkgs = true;
-            users.mdray = {self, pkgs, lib, config, inputs, options,...}: {
-              home.stateVersion = "22.11";
-            };
-            # users.mdray = import ./home/default.nix;
+            users.mdray = {inherit ./home;};
           };
         }
       ];
