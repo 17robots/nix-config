b94e3e9d25c64232b135ff6baddc048f788a725b
17robots <mdray@ameritech.net>
2023-05-05 00:36:48 -0400

    fix: inputs inherit

--- flake.nix
+++ flake.nix
@@ -7,22 +7,19 @@
     nixos-hardware.url = "github:Nixos/nixos-hardware/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs, home-manager, nixos-hardware }: let 
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
+  outputs = { self, nixpkgs, home-manager, nixos-hardware }: {
     nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
         nixos-hardware.nixosModules.dell-xps-15-9500
       ];
-      specialArgs = {inherit inputs;};
+      specialArgs = {inherit (self) inputs};
     };
 
     homeConfigurations.mdray = home-manager.lib.homeManagerConfiguration {
       modules = [ ./home ];
-      specialArgs = {inherit inputs;};
+      specialArgs = {inherit (self) inputs};
     };
   };
 }
